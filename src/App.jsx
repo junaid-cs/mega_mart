2a24b22da813e960aa27c2a05f18f7bde4d49ff7
Jay <85805386+junaid-cs@users.noreply.github.com>
2023-08-14 20:57:41 +0500

    adding framer motion

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,33 @@
-import { useEffect, useState } from 'react'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
 import AddtoCart from './component/AddtoCart/AddtoCart';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchdata } from './store/Slices/UserSlice';
 import StarRating from './component/StarRating/StarRating';
-
-
+import { motion } from 'framer-motion';
 
 function App() {
   const { data, loading, error } = useSelector(state => state.user);
-  const { filteredProducts, searchValue } = useSelector(state => state.filter)
+  const { filteredProducts, searchValue } = useSelector(state => state.filter);
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   useEffect(() => {
-    let url = "https://fakestoreapi.com/products"
-    dispatch(fetchdata(url))
+    let url = "https://fakestoreapi.com/products";
+    dispatch(fetchdata(url));
   }, []);
-  // screen width
+
   const [screenWidth, setScreenWidth] = useState(window.innerWidth);
   useEffect(() => {
-    // Update screenWidth when window is resized
     const handleResize = () => {
       setScreenWidth(window.innerWidth);
     };
 
     window.addEventListener('resize', handleResize);
 
-    // Clean up the event listener on component unmount
     return () => {
       window.removeEventListener('resize', handleResize);
     };
-
   }, []);
-  // console.log(screenWidth);
-
-
-  // stars
-
-
-
-
-
 
   const filteredData = data
     .filter(row => {
@@ -50,14 +37,19 @@ function App() {
       return isMatchingCategory && isMatchingSearch;
     })
     .map(item => (
-      <div className="col-10 col-sm-6 col-md-3 col-lg-3 mb-5 p-0" key={item.id}>
-        <div className="card align-items-center w-100 h-100 ">
+      <motion.div
+        key={item.id}
+        className="col-10 col-sm-6 col-md-3 col-lg-3 mb-5 p-0"
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.7 }}
+      >
+        <div className="card align-items-center w-100 h-100">
           <div className='img_container position-relative w-100 d-flex justify-content-center align-items-center'>
             <img src={item.image} style={{ width: '84px', height: '158px' }} className="card-img-top -centertext object-fit-contain" alt="..." />
             {
               screenWidth > 1000 ?
                 (
-
                   <div className="overlay position-absolute ">
                     <AddtoCart item={item} />
                   </div>
@@ -65,33 +57,27 @@ function App() {
                 : null
             }
           </div>
-          <div className="card-body bg-white  w-100 ">
+          <div className="card-body bg-white w-100 ">
             <div className="d-sm-flex">
-            <div className='card_title'>{item.title.split(' ').slice(0, 3).join(' ')}</div>
-            <div className="card_price">${item.price}</div>
+              <div className='card_title'>{item.title.split(' ').slice(0, 3).join(' ')}</div>
+              <div className="card_price">${item.price}</div>
             </div>
             <div className="card_rating d-flex justify-content-between align-items-center">
               <StarRating rating={item.rating.rate} />
               {
-              screenWidth <= 1000 ?
-                (
-
-                  <div className="">
-                    <AddtoCart item={item} />
-                  </div>
-                )
-                : null
-            }
+                screenWidth <= 1000 ?
+                  (
+                    <div className="">
+                      <AddtoCart item={item} />
+                    </div>
+                  )
+                  : null
+              }
             </div>
           </div>
         </div>
-      </div>
+      </motion.div>
     ));
-
-
-
-
-
 
   return (
     <>
@@ -99,9 +85,7 @@ function App() {
         {filteredData}
       </div>
     </>
-  )
+  );
 }
 
-export default App
-
-
+export default App;
